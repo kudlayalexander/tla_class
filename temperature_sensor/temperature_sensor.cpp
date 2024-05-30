ad5bab2a7216853e3c464fbca54b5d0a89bfb2ce
kudlayalexander <johndoe@example.com>
2024-05-30 22:11:34 +0500

    implemented temp and resist calculation

--- temperature_sensor/temperature_sensor.cpp
+++ temperature_sensor/temperature_sensor.cpp
@@ -1,4 +1,4 @@
-#include "temperature-sensor.h"
+#include "temperature_sensor.h"
 
 float TemperatureSensor::getTemperature() {
     return calculateTemperature();
@@ -13,13 +13,19 @@ float TemperatureSensor::getVoltage() {
 };
 
 float TemperatureSensor::calculateTemperature() {
+    // https://colab.research.google.com/drive/1yGktFqW0doqNn0V1L1ZOb1zqJbIHToTf?usp=sharing
     float linearRegressionCoeffs[7];
 
+
+    
     return calculateValueByWeights(linearRegressionCoeffs, voltage);
 };
 
 float TemperatureSensor::calculateResistance() {
-    float linearRegressionCoeffs[11];
+    // https://colab.research.google.com/drive/1yGktFqW0doqNn0V1L1ZOb1zqJbIHToTf?usp=sharing
+    float linearRegressionCoeffs[11] = {[ 0, -3.48377576e+09f,  9.57536152e+09f, -1.53773476e+10f,
+        1.59803002e+10f, -1.12309086e+10f,  5.40719448e+09f, -1.76155319e+09f,
+        3.71766254e+08f, -4.59154778e+07f,  2.52123921e+06f]}
 
     return calculateValueByWeights(linearRegressionCoeffs, voltage);
 };
